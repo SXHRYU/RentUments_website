eecbcfca7e88301b3b80e8209f09337bb2c4e960
Slava Maltsev <slavaxru@gmail.com>
2022-02-23 19:31:00 +0300

    added email

--- products/forms.py
+++ products/forms.py
@@ -12,19 +12,26 @@ class ProductForm(forms.ModelForm):
                             'maxlength': 100, 
                             'placeholder': 'max length = 100'}
                             ))
+    email       = forms.EmailField()
     class Meta:
         model = Product
         fields = [
             'title', 
             'description', 
             'price',
-            'featured',
+            'email',
         ]
+    
     def clean_title(self, *args, **kwargs):
         title = self.cleaned_data.get('title')
         if not 'CFE' in title:
             raise forms.ValidationError('This is not a valid title')
         return title
+
+    def clean_email(self, *args, **kwargs):
+        email = self.cleaned_data.get('email')
+        if not email.endswith('edu'):
+            raise forms.ValidationError('This is not a valid email')
 
 class RawProductForm(forms.Form):
     title       = forms.CharField()
