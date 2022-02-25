708443d68ecda3e373999961dc8eea0f70986397
Slava Maltsev <slavaxru@gmail.com>
2022-02-25 11:50:34 +0300

    .gitignore is now working

--- products/views.py
+++ products/views.py
@@ -39,3 +39,9 @@ def product_detail_view(request, *args, **kwargs):
     }
     return render(request, 'products/product_detail.html', context)
 
+def dynamic_lookup_view(request):
+    obj = Product.objects.get(id=37)
+    context = {
+        'object': obj
+    }
+    return render(request, 'products/product_detail.html', context)
